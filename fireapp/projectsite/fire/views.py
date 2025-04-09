f048e9f13843ab103323cb6a8472ddcb26739739
CharlesJazon <202080041@psu.palawan.edu.ph>
2025-04-09 20:14:18 +0800

    successful chart setup

--- fireapp/projectsite/fire/views.py
+++ fireapp/projectsite/fire/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.views.generic.list import ListView, CreateView
 from fire.models import Locations, Incident
 
 
@@ -7,3 +7,14 @@ class HomePageView(ListView):
     model = Locations
     context_object_name = 'home'
     template_name = "home.html"
+    
+class ChartView(ListView):
+    template_name = 'chart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        pass
+
