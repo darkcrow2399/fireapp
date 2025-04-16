d19727bb0c60cfe8abce051df3a86d37b8ac00c3
Andrew <23andrew99@gmail.com>
2025-04-17 02:22:37 +0800

    edited settings.py

--- fireapp/projectsite/projectsite/settings.py
+++ fireapp/projectsite/projectsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-d@4a$whi@(x03p+^ccwzokc$xcs*)qez!1(p_bdyvq1ebk6!3e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['penafireapp.pythonanywhere.com']
 
 
 # Application definition
